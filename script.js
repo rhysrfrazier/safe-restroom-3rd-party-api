faba00cb5eee0dd6dfebf9b2d8da23cf0128e74b
Rhys Frazier <rhysrfrazier@gmail.com>
2023-09-25 16:08:22 -0400

    got a js function working to remove cards

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-// function addCard(){
-//     const footer = document.querySelector("#more")
-//     const resultCard = document.createElement("div") //can i add .className and just do that at the same time here?
-//     const left = document.createElement("div")
-//     const h2 = document.createElement("h2")
-//     const generatedName = document.createTextNode("Place Name Variable")
-//     const addPlace = h2.appendChild(generatedName)
-//     const placeName = left.appendChild(addPlace)
-//     resultCard.appendChild(placeName) 
-//     //const right = document.createElement("div")
-// }
-// addCard()
+const submitBtn = document.querySelector("#submit")
 
-function addCard(){
+function addCard(placeVar, addressVar, accessVar, unisexVar, changingVar){
     //creating the overall div and setting it's class for styling
     const resultCard = document.createElement("div")
     resultCard.classList.add("resultCard")
@@ -23,12 +12,12 @@ function addCard(){
     //adding the placeName section and attaching it to left
     const h2 = document.createElement("h2")
     h2.setAttribute("id", "placeName")
-    h2.innerHTML= "Place name var"
+    h2.innerHTML= placeVar
     left.appendChild(h2)
     //adding the address section and attaching it to left
     const h3 = document.createElement("h3")
     h3.setAttribute("id", "address")
-    h3.innerHTML="Address var"
+    h3.innerHTML=addressVar
     left.appendChild(h3)
     //creating the "right" div and attaching it to the resultCard
     const right = document.createElement("div")
@@ -38,19 +27,30 @@ function addCard(){
     const access = document.createElement("h4")
     access.classList.add("options")
     access.setAttribute("id", "accessible")
-    access.innerHTML = "Accessible y/n var"//create strings for each option that will put the innerHTML you need in here, but make those later in the api call section
+    access.innerHTML = accessVar//create strings for each option that will put the innerHTML you need in here, but make those later in the api call section... each variable in here needs to correspond to a perameter that I can pass the variables in to
     right.appendChild(access)
     const unisex = document.createElement("h4")
     unisex.classList.add("options")
     unisex.setAttribute("id", "unisex")
-    unisex.innerHTML = "Unisex y/n var"
+    unisex.innerHTML = unisexVar
     right.appendChild(unisex)
     const changingStation = document.createElement("h4")
     changingStation.classList.add("options")
     changingStation.setAttribute("id", "changingStation")
-    changingStation.innerHTML = "Changing Station y/n var"
+    changingStation.innerHTML = changingVar
     right.appendChild(changingStation)
     //add the whole card to the body
     document.body.appendChild(resultCard)
 }
-addCard()
+addCard("Place", "XXXX Place, Charlottesville, VA XXXXX", "not accessible", "yes unisex", "no changing station")
+addCard("Not-house", "6796 Sugar Hollow Rd, Crozet VA 22932", "not accessible", "yes unisex", "no changing station")
+
+const resultCards = document.querySelectorAll(".resultCard")//this MUST go after the addCard function call to pick up the cards
+console.log(resultCards)
+
+function removeCards(){
+   resultCards.forEach((card) => card.remove())
+}
+removeCards()
+
+
